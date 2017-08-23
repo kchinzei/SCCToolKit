0e7c1730990a9f4ffd5f65bacd4c3e24fc449170
kchinzei <scc-contact-ml@aist.go.jp>
2017-08-23 16:14:22 +0900

    FindDeckLinkSDK.cmake as a standalone project
    Modified FindDeckLinkSDK.cmake to be independent from other SCCToolKit
    files.
     - More strict file existence checks.
     - Optional version check.
     - DeckLinkSDK_INCLUDE_PATH, DeckLinkSDK_LIBS, DeckLinkSDK_SRC_FILE are
    chached.

--- Cmake/FindDeckLinkSDK.cmake
+++ Cmake/FindDeckLinkSDK.cmake
@@ -12,43 +12,82 @@
 #  DeckLinkSDK_LIBS     = external libraries to link for target.
 #  DeckLinkSDK_DEF      = additional compiler flags.
 #
-# If a system environment variable either of DECKLINK_SDK_DIR, BLACKMAGIC_SDK_DIR, DECKLINK_DIR or BLACKMAGIC_DIR set, it looks at paths from them.
+# If a system environment variable either of DECKLINK_SDK_DIR, BLACKMAGIC_SDK_DIR, DECKLINK_DIR or BLACKMAGIC_DIR set,
+# it looks at paths from them.
 # Typical CmakeLists.txt snippet is;
+#    FIND_PACKAGE(DeckLinkSDK REQUIRED)
 #    IF(DeckLinkSDK_FOUND)
 #      INCLUDE_DIRECTORIES("${DeckLinkSDK_INCLUDE_PATH}")
-#      SET(COMPILE_DEFINITIONS "${COMPILE_DEFINITIONS};${DeckLinkSDK_DEF}")
-#      TARGET_LINK_LIBRARIES(${your_app} ${your_libs} "${DeckLinkSDK_LIBS}")
+#      IF   (COMPILE_DEFINITIONS)
+#        SET(COMPILE_DEFINITIONS "${COMPILE_DEFINITIONS};${DeckLinkSDK_DEF}")
+#      ELSE (COMPILE_DEFINITIONS)
+#        SET(COMPILE_DEFINITIONS ${DeckLinkSDK_DEF})
+#      ENDIF(COMPILE_DEFINITIONS)
 #      ADD_EXECUTABLE(${your_app} ${your_srcs} "${DeckLinkSDK_SRC_FILE}")
+#      TARGET_LINK_LIBRARIES(${your_app} ${your_libs} "${DeckLinkSDK_LIBS}")
 #    ENDIF(DeckLinkSDK_FOUND)
 # You do the following in your DeckLink related sources;
-#    #include "DeckLinkSDK_INCLUDE_FILE"
+#    #include "DeckLinkSDK.h"
+#
+# You can optionally provide version argument, for example;
+#    FIND_PACKAGE(DeckLinkSDK 10.5 REQUIRED)
+# will ensure the version of the SDK is greater than 10.0 (e.g., 10.4.99 will produce an error.)
 
 #=============================================================================
 # (c) Kiyoyuki Chinzei, AIST japan.
 # Small Computings for Clinicals
-# Do not remove this copyright claim and the condition of the use.
-# Use, modify and distribute it freely, but at your own risk.
+# Do not remove this copyright claim and the license conditions below.
+# This file is released under two licenses: the BSD 3-Clause license and the MIT license.
+# See https://opensource.org/licenses
+# You may pick the license that best suits your needs.
 #=============================================================================
 
+# _resolve_backslash
+#
+# Resolve backslashes in ${variable} to properly work on path expression. It assumes 3 cases:
+#  1) set(variable "c:\\foo\\hoge")
+#  2) set(variable "c:\foo\hoge")
+#  3) set(variable "/foo/hoge\ hoge")
+# For these cases, scc_resolve_backslash(variable) will resolve
+#  1) "c:/foo/hoge"
+#  2) "c:/foo/hoge"
+#  3) "/foo/hoge hoge"
+# accordingly. It does not check valiable is valid.
+function(_resolve_backslash variable)
+  set(_tmpv ${${variable}})
+  string(REPLACE "\\\\" "/" _tmpv ${_tmpv})
+  string(REPLACE "\\ " " " _tmpv ${_tmpv})
+  string(REPLACE "\\" "/" _tmpv ${_tmpv})
+  set(${variable} "${_tmpv}" PARENT_SCOPE)
+endfunction()
+
+#
+# Main code of this module
+#
 SET(DeckLink_DEFAULT "/src/Blackmagic_SDK")
 
-SET(DeckLinkSDK_INCLUDE_PATH "")
-SET(DeckLinkSDK_INCLUDE_FILE "")
-SET(DeckLinkSDK_SRC_FILE "")
-SET(DeckLinkSDK_LIBS "")
-SET(DeckLinkSDK_DEF "")
+#UNSET(DeckLinkSDK_FOUND)
+#SET(DeckLinkSDK_INCLUDE_PATH "")
+#SET(DeckLinkSDK_INCLUDE_FILE "")
+#SET(DeckLinkSDK_SRC_FILE "")
+#SET(DeckLinkSDK_DEF "")
+#UNSET(DeckLinkSDK_INCLUDE_PATH CACHE)
+#UNSET(DeckLinkSDK_INCLUDE_FILE CACHE)
+#UNSET(DeckLinkSDK_SRC_FILE CACHE)
+#UNSET(DeckLinkSDK_LIBS CACHE)
+#UNSET(DeckLinkSDK_DEF)
 
 IF(WIN32)
-    SET(DeckLinkSDK_INCLUDE_FILE DeckLinkAPI.idl)
-    SET(DeckLinkSDK_INCLUDE_PATH Win/include)
+    SET(__include_file DeckLinkAPI.idl)
+    SET(__include_path Win/include)
 ELSE(WIN32)
-    SET(DeckLinkSDK_SRC_FILE DeckLinkAPIDispatch.cpp)
-    SET(DeckLinkSDK_INCLUDE_FILE DeckLinkAPI.h)
+    SET(__src_file DeckLinkAPIDispatch.cpp)
+    SET(__include_file DeckLinkAPI.h)
     IF(APPLE)
-	SET(DeckLinkSDK_INCLUDE_PATH Mac/include)
+	SET(__include_path Mac/include)
 	FIND_LIBRARY(DeckLinkSDK_LIBS CoreFoundation)
     ELSE(APPLE)
-	SET(DeckLinkSDK_INCLUDE_PATH Linux/include)
+	SET(__include_path Linux/include)
 	SET(DeckLinkSDK_LIBS "")
     ENDIF(APPLE)
 ENDIF(WIN32)
@@ -56,50 +95,46 @@ MARK_AS_ADVANCED(FORCE DeckLinkSDK_LIBS)
 
 # We attempt to examine with or without ${DeckLink_DEFAULT}
 # We need to tempolariy unset ${DeckLinkSDK_PATH} cache.
-if(DeckLinkSDK_PATH)
-  scc_resolve_backslash(DeckLinkSDK_PATH)
-  SET(DeckLinkSDK_PATH_TMP "${DeckLinkSDK_PATH}")
-else(DeckLinkSDK_PATH)
-  SET(DeckLinkSDK_PATH_TMP "${DeckLink_DEFAULT}")
-endif(DeckLinkSDK_PATH)
+IF(DeckLinkSDK_PATH)
+  SET(decklinksdk_path_tmp "${DeckLinkSDK_PATH}")
+ELSEIF(DeckLink_DEFAULT)
+  SET(decklinksdk_path_tmp "${DeckLink_DEFAULT}")
+ENDIF(DeckLinkSDK_PATH)
+_resolve_backslash(decklinksdk_path_tmp)
 
 UNSET(DeckLinkSDK_PATH CACHE)
 FIND_PATH(DeckLinkSDK_PATH
-    ${DeckLinkSDK_INCLUDE_PATH}/${DeckLinkSDK_INCLUDE_FILE}
-    PATHS "${DeckLinkSDK_PATH_TMP}" ENV DECKLINK_SDK_DIR ENV BLACKMAGIC_SDK_DIR ENV DECKLINK_DIR ENV BLACKMAGIC_DIR
+    ${__include_path}/${__include_file}
+    PATHS "${decklinksdk_path_tmp}" ENV DECKLINK_SDK_DIR ENV BLACKMAGIC_SDK_DIR ENV DECKLINK_DIR ENV BLACKMAGIC_DIR
     DOC "Where DeckLinkSDK locates."
    )
-UNSET(DeckLinkSDK_PATH_TMP)
 
-IF(EXISTS "${DeckLinkSDK_PATH}/${DeckLinkSDK_INCLUDE_PATH}/${DeckLinkSDK_INCLUDE_FILE}")
-    IF(DeckLinkSDK_SRC_FILE)
-	SET(DeckLinkSDK_SRC_FILE "${DeckLinkSDK_PATH}/${DeckLinkSDK_INCLUDE_PATH}/${DeckLinkSDK_SRC_FILE}")
-    ENDIF(DeckLinkSDK_SRC_FILE)
-    SET(DeckLinkSDK_INCLUDE_PATH "${DeckLinkSDK_PATH}/${DeckLinkSDK_INCLUDE_PATH}")
-    SET(DeckLinkSDK_PATH
-      "${DeckLinkSDK_PATH}"
-      CACHE PATH "Where DeckLinkSDK locates.")
-#    MARK_AS_ADVANCED(
-#      DeckLinkSDK_INCLUDE_PATH
-#      DeckLinkSDK_INCLUDE_FILE
-#      DeckLinkSDK_SRC_FILE)
-ELSE()    
-    UNSET(DeckLinkSDK_INCLUDE_PATH)
-    UNSET(DeckLinkSDK_SRC_FILE)
-    UNSET(DeckLinkSDK_LIBS)
-    UNSET(DeckLinkSDK_DEF)
-    UNSET(DeckLinkSDK_PATH CACHE)
-    SET(DeckLinkSDK_PATH
-      DeckLinkSDK-NOTFOUND
-      CACHE PATH "Where DeckLinkSDK locates.")
+FIND_PATH(DeckLinkSDK_INCLUDE_PATH ${__include_file} PATHS "${DeckLinkSDK_PATH}" PATH_SUFFIXES ${__include_path})
+SET(DeckLinkSDK_INCLUDE_FILE ${__include_file})
+IF(__src_file)
+  FIND_FILE(DeckLinkSDK_SRC_FILE ${__src_file} PATHS "${DeckLinkSDK_INCLUDE_PATH}")
+ENDIF(__src_file)
+
+MARK_AS_ADVANCED(DeckLinkSDK_INCLUDE_PATH DeckLinkSDK_SRC_FILE)
+
+SET(version.h "${DeckLinkSDK_INCLUDE_PATH}/DeckLinkAPIVersion.h")
+IF(EXISTS "${version.h}")
+  FILE(STRINGS "${version.h}" _tmpstr REGEX "^#define[\t ]+BLACKMAGIC_DECKLINK_API_VERSION_STRING[\t ]+\".*\"")
+  STRING(REGEX REPLACE                      "^#define[\t ]+BLACKMAGIC_DECKLINK_API_VERSION_STRING[\t ]+\"([^\"]*)\".*" "\\1"
+    DeckLinkSDK_VERSION_STRING "${_tmpstr}")
 ENDIF()
 
-# Handle the QUIETLY and REQUIRED arguments and set DeckLinkSDK_FOUND to TRUE if 
-# all listed variables are TRUE
-
 INCLUDE(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(
-  DeckLinkSDK 
-  DEFAULT_MSG
-  DeckLinkSDK_PATH)
-SET(DeckLinkSDK_FOUND ${DECKLINKSDK_FOUND})
+IF(__src_file)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+    DeckLinkSDK
+    REQUIRED_VARS DeckLinkSDK_PATH DeckLinkSDK_INCLUDE_PATH DeckLinkSDK_SRC_FILE
+    VERSION_VAR DeckLinkSDK_VERSION_STRING
+    FAIL_MESSAGE "DeckLink SDK not found. Usually in 'Blackmagic DeckLink SDK *.*.*'.")
+ELSE()
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+    DeckLinkSDK
+    REQUIRED_VARS DeckLinkSDK_PATH DeckLinkSDK_INCLUDE_PATH
+    VERSION_VAR DeckLinkSDK_VERSION_STRING
+    FAIL_MESSAGE "DeckLink SDK not found. Usually in 'Blackmagic DeckLink SDK *.*.*'.")
+ENDIF()
--- Examples/capture_cv1/readme.txt
+++ Examples/capture_cv1/readme.txt
@@ -4,10 +4,10 @@ image in a window. It uses a for-loop to continuously capture and
 update the view. Compare this with capture_cv2, which is timer driven
 and programmed in concurrent way.
 
-Options:
- [-w width]    default=320 : The width of the caputured image.
- [-h height]   default=240 : The height of the captured image.
- [-f FPS]      default=30  : Frame per second, but it has no effect.
- [cameraid]    default=0   : Specify which camera to open.
+Options: option flag and number connected by ‘=‘
+ [-w, -—width]  default=320 : The width of the captured image.
+ [-h, —-height] default=240 : The height of the captured image.
+ [-f, —-FPS]    default=30  : Frame per second, but it has no effect.
+ [cameraid]     default=0   : Specify which camera to open.
 
 
